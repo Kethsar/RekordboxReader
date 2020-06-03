a2df10a9932674ed9c7ad34c0c4ad7f40b56c111
Kethsar <kethstuff@gmail.com>
2020-06-03 03:11:49 -0700

    Version bump

--- RekordboxReader/Properties/AssemblyInfo.cs
+++ RekordboxReader/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("r/a/dio @ Rizon, ed and drinks INC")]
 [assembly: AssemblyProduct("RekordboxReader")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © ryuu, Kethsar 2020")]
 [assembly: AssemblyTrademark("Gayest channel on Rizon 8 years running")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
